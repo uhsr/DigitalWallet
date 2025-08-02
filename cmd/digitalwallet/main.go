df8bc8d3b96dd65b7be3bb3f665e5f765182d8e0
uhsr <uhsr@eteb.me>
2025-08-02 23:28:16 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/digitalwallet/main.go
@@ -0,0 +1,23 @@
+// cmd/digitalwallet/main.go
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"digitalwallet/internal/digitalwallet"
+)
+
+func main() {
+	verbose := flag.Bool("verbose", false, "Enable verbose logging")
+	input := flag.String("input", "", "Input file path")
+	output := flag.String("output", "", "Output file path")
+	flag.Parse()
+
+	app := digitalwallet.NewApp(*verbose)
+	if err := app.Run(*input, *output); err != nil {
+		log.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+}
